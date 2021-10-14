baa38757b98c3f51115160ec7c338dfe6d10dfe6
chikwado-ezenwaka <kwado21@gmail.com>
2021-10-14 19:40:16 +0200

    updated task 20 - 22

--- JavaArrays.java
+++ JavaArrays.java
@@ -1,5 +1,33 @@
 package com.company;
 
-public class JavaArrays {
+import java.util.ArrayList;
+import java.util.Arrays;
 
+public class JavaArrays {
+    public static void main(String[] args) {
+        int [] array1 = new int [9];
+        array1[0] =10;
+        array1[1] =1;
+        array1[2] =4;
+        array1[3] =2;
+        //System.out.println(Arrays.toString(array1));
+
+        String [] fruits = {"mango" ,"apple" ,"guava"};
+      //  System.out.println(Arrays.toString(fruits));
+
+        int [] [] doubleDimen = new int [2] [3];
+        doubleDimen[0] [0] = 20;
+        doubleDimen [0] [0] = 30;
+        //System.out.println(Arrays.deepToString(doubleDimen));
+
+        //ArrayList
+        ArrayList<String> arr = new ArrayList<String>();
+        arr.add("test");
+        arr.add("10");
+
+        //arr.remove(1);
+        System.out.println(arr.get(1));
+
+        //ASSIGNMENT
+    }
 }
---
+++ evenJavaLOOP.java
@@ -0,0 +1,10 @@
+public class evenJavaLOOP {
+
+        public static void main(String[] args) {
+            //even number
+            for (int i =0; i <=10; i++){
+                if (i%2==0){
+                    System.out.println(i);
+                }}}}
+
+
